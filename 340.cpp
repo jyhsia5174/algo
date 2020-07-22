07030bbb7b41fa5d490dd79be0139da6c9ca533e
Hsia, Jui-Yang <johncreed@users.noreply.github.com>
2020-07-22 13:39:44 +0800

    Update 340.cpp

--- 340.cpp
+++ 340.cpp
@@ -2,57 +2,40 @@
 class Solution {
 public:
     int lengthOfLongestSubstringKDistinct(string s, int k) {
-        if( k == 0 ) return 0;
-        if( s.size() <= k ) return s.size();
-        for(int i = 0; i < 300; i++)
+        for(int i = 0; i < 256; i++)
             count[i] = 0;
         
-        int l = 0, r = 0;
-        while( r < s.size() && unique <= k ){
-            add( s[r] );
-            r++;
-        }
-        
-        int L;
-        if( unique <= k )
-            L = r - l;
-        else
-            L = r - l - 1;
+        int l = 0, r = 0, L = 0;
         
         while( r < s.size() ){
-            while( unique > k ){
+            if( unique <= k ){
+                L = max(L, r - l);
+                add(s[r]);
+                r++;
+            }
+            else{
                 remove(s[l]);
                 l++;
             }
-            
-            while( r < s.size() && unique <= k ){
-                add( s[r] );
-                r++;
-            }
-            
-            int L_tmp;
-            if( unique <= k )
-                L_tmp = r - l;
-            else
-                L_tmp = r - l - 1;
-            L = max( L, L_tmp );
         }
+        
+        if( unique <= k ) L = max( L, r - l );
         
         return L;
     }
 private:
-    int count[300];
+    int count[256];
     int unique = 0;
     
     void add(const char &c ){
-        int cid = (unsigned char) c;
+        int cid = c;
         if( count[cid] == 0 )
             unique++;
         count[cid]++;
     }
     
     void remove(const char &c ){
-        int cid = (unsigned char) c;
+        int cid = c;
         count[cid]--;
         if( count[cid] == 0 )
             unique--;
