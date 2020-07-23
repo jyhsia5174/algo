5739741c9b105e3618c277437c1cfefb536cd45e
Hsia, Jui-Yang <johncreed@users.noreply.github.com>
2020-07-23 13:04:26 +0800

    Update 523.cpp

--- 523.cpp
+++ 523.cpp
@@ -2,36 +2,19 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        vector<unsigned int> sum;
-        const int N = nums.size();
-        if( N < 2 ) return false;
-        
-        sum.resize( N+1, 0 );
-        
-        k = abs(k);
-        
-        if( k == 0 ){
-            int ii = 0, jj = 0;
-            while( ii <= jj && jj < nums.size() ){
-                if( jj - ii >= 2 ) return true;
-                if( nums[jj] == 0 ) jj++;
-                else ii = jj = jj+1;
-            }
-            if( jj - ii >= 2 ) return true;
-            return false;
-        }
-        
-        unordered_map<int, int> S;
+        unordered_map<int, int> m;
+        m[0] = -1;
+        int sum = 0, mode;
         for(int i = 0; i < nums.size(); i++){
-            sum[i+1] = ( sum[i] + (unsigned int) nums[i] ) % k;
-            S[sum[i+1]]++;
-        }
-        
-        S[sum[1]]--;
-        if( S[0] >= 1 ) return true;
-        for(int i = 0; i + 1 < N; i++){
-            if( S[sum[i]] >= 1 ) return true;
-            S[sum[i+2]]--;
+            sum += nums[i];
+            if( k == 0 )
+                mode = sum;
+            else
+                mode = sum%k;
+            
+            if( m.count(mode) && i - m[mode] >= 2 ) return true;
+            
+            if( !m.count(mode) ) m[mode] = i;
         }
         
         return false;
