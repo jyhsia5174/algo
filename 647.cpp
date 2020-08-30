e6680ae585a2643c384b863f4fbccfba39e6ec21
Hsia, Jui-Yang <johncreed@users.noreply.github.com>
2020-08-30 15:33:35 +0800

    Update 647.cpp

--- 647.cpp
+++ 647.cpp
@@ -5,6 +5,35 @@ public:
         const int n = s.size();
         if( n == 0 ) return 0;
         
+        // [i-d, i+d]
+        vector<int> d(2*n+1, 0);
+        int center = 0, right = 0;
+        for(int i = 0; i < 2*n+1; i++){
+            if( i < right )
+                d[i] = min(right - i, d[2*center-i]);
+            while( ((i+d[i]-1)/2+1) < n && ((i-d[i])/2-1) >= 0
+                  && s[(i+d[i]-1)/2+1] == s[(i-d[i])/2-1] )
+                d[i] += 2;
+            if( i % 2 == 1 && d[i] % 2 == 0 ) d[i]++;
+            if( i + d[i] > right ){
+                center = i;
+                right = i + d[i];
+            }
+        }
+        
+        int ans = 0;
+        for(auto &v: d)
+            ans += (v+1)/2;
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int countSubstrings(string s) {
+        const int n = s.size();
+        if( n == 0 ) return 0;
+        
         int count = 0;
         queue<int> Q;
         int idx = 0;
