7bf1deb597ad548a48c2f2951e45512806f1a6e9
Hsia, Jui-Yang <johncreed@users.noreply.github.com>
2020-09-04 15:38:03 +0800

    Update 474.cpp

--- 474.cpp
+++ 474.cpp
@@ -28,3 +28,45 @@ public:
         return {zero, one};
     }
 };
+
+
+class Solution {
+public:
+    vector<vector<vector<int>>> dp;
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        int L = strs.size();
+        dp.resize(L);
+        for(int i = 0; i < L; i++)
+            dp[i] = vector<vector<int>> (m+1, vector<int>(n+1, 0) );
+        
+        return recursive(strs, 0, m, n);
+    }
+    
+    int recursive(vector<string>& strs, int i, int m, int n){
+        if( strs.size() == i )
+            return 0;
+        if( dp[i][m][n] != 0 )
+            return dp[i][m][n];
+        
+        auto [zero, one] = parse(strs[i]);
+        int c1 = recursive(strs, i+1, m, n);
+        int c2 = 0;
+        if( m >= zero && n >= one )
+            c2 = recursive(strs, i+1, m-zero, n-one) + 1;
+        
+        dp[i][m][n] = max(c1, c2);
+        return dp[i][m][n];
+    }
+    
+    pair<int, int> parse(const string &s){
+        int zero = 0, one = 0;
+        for(int i = 0; i < s.size(); i++){
+            if( s[i] == '0' )
+                zero++;
+            else
+                one++;
+        }
+        
+        return {zero, one};
+    }
+};
