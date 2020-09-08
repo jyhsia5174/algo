c555573831ad14cce6f601ad035534a39d34edbf
Hsia, Jui-Yang <johncreed@users.noreply.github.com>
2020-09-08 15:21:03 +0800

    Update 887.cpp

--- 887.cpp
+++ 887.cpp
@@ -1,5 +1,36 @@
 // 887. Super Egg Drop
 
+// dp
+
+class Solution {
+public:
+    int superEggDrop(int K, int N) {
+        int dp[K+1][N+1];
+        
+        for(int i = 1; i <= K; i++){
+            dp[i][0] = 0;
+            dp[i][1] = 1;
+        }
+        
+        for(int j = 1; j <= N; j++)
+            dp[1][j] = j;
+        
+        for(int i = 2; i <= K; i++){
+            for(int j = 2; j <= N; j++){
+                dp[i][j] = INT_MAX;
+                for(int k = 1; k <= j; k++){
+                    int res = max( dp[i-1][k-1], dp[i][j-k] );
+                    if( res < dp[i][j] )
+                        dp[i][j] = res;
+                }
+                dp[i][j] += 1;
+            }
+        }
+        
+        return dp[K][N];
+    }
+};
+
 // 
 
 class Solution {
