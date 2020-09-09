6a8288088382b5403c3dcfd6b2fb2b0bbc62c35a
Hsia, Jui-Yang <johncreed@users.noreply.github.com>
2020-09-09 14:45:49 +0800

    Update 343.cpp

--- 343.cpp
+++ 343.cpp
@@ -19,3 +19,27 @@ public:
 };
 
 // recursive with memorization
+class Solution {
+public:
+    int integerBreak(int n) {
+        cache.resize(n+1, 0);
+        return MP(n);
+    }
+    
+    vector<int> cache;
+    
+    int MP(int n){
+        if( n == 1 ) return 0;
+        if( cache[n] != 0 ) return cache[n];
+        
+        int max_prod = 0;
+        int prod;
+        for(int i = 1; i < n; i++){
+            prod = max( i * (n-i), i * MP(n-i) );
+            max_prod = max(max_prod, prod);
+        }
+        
+        cache[n] = max_prod;
+        return cache[n];
+    }
+};
