c58716fd5b4106faf79f9c0b6e921548cb11d0c7
Hsia, Jui-Yang <johncreed@users.noreply.github.com>
2020-09-22 07:52:37 +0800

    Update 1.cpp

--- java/1.cpp
+++ java/1.cpp
@@ -1,4 +1,6 @@
 // 1. Two Sum
+
+// O(n^2)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for( int i = 0; i < nums.length; i++){
@@ -7,6 +9,27 @@ class Solution {
                     return new int[] {i, j};
             }
         }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+// O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap();
+        
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if( map.containsKey(complement) 
+               && map.get(complement) != i ){
+                return new int[] { i, map.get(complement)};
+            }
+        }
+        
         throw new IllegalArgumentException("No two sum solution");
     }
 }
