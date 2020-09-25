dd08449d0fd323e78fbbbd632e9265d369c7a2d3
Hsia, Jui-Yang <johncreed@users.noreply.github.com>
2020-09-25 09:40:28 +0800

    Update 234.java

--- java/234.java
+++ java/234.java
@@ -43,6 +43,62 @@ class Solution {
     }
 }
 
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if( head == null )
+            return true;
+        
+        ListNode halfNode = getHalfNode(head);
+        ListNode reverseHead = reverse(halfNode.next);
+        
+        ListNode p1 = head;
+        ListNode p2 = reverseHead;
+        while( p2 != null ){
+            if( p1.val != p2.val )
+                return false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        
+        return true;
+    }
+    
+    private ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while( curr != null ){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    private ListNode getHalfNode(ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+        while( fast.next != null && fast.next.next != null ){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        return slow;
+    }
+}
+
+
 // time O(n), space O(n)
 /**
  * Definition for singly-linked list.
@@ -71,3 +127,6 @@ class Solution {
         return recursivelyCheck(head);
     }
 }
+
+
+
