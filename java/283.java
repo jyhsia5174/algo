a5dd687c6380dcbbf0ba9156aa570f17b47c61d1
Hsia, Jui-Yang <johncreed@users.noreply.github.com>
2020-09-27 09:48:25 +0800

    Create 283.java

---
+++ java/283.java
@@ -0,0 +1,18 @@
+// 283. Move Zeroes
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            if( nums[i] != 0 ){
+                if( i != j ){
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
+                }
+                j++;
+            }
+        }
+        
+    }
+}
