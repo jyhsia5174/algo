4589b60174649d6b853c680b650b9e0158183eeb
Hsia, Jui-Yang <johncreed@users.noreply.github.com>
2020-10-02 09:40:51 +0800

    Create 387.java

---
+++ java/387.java
@@ -0,0 +1,34 @@
+// 387. First Unique Character in a String
+// Time : O(n) Space: O(1)
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] first_idx = new int[26];
+        int[] count = new int[26];
+        
+        for(int i = 0; i < 26; i++){
+            first_idx[i] = -1;
+            count[i] = 0;
+        }
+        
+        for(int i = 0; i < s.length(); i++){
+            int hash = s.charAt(i)-'a';
+            if( count[hash] == 0 ){
+                first_idx[hash] = i;
+            }
+            
+            count[hash]++;
+        }
+        
+        
+        int min_idx = Integer.MAX_VALUE;
+        for(int i = 0; i < 26; i++){
+            if( count[i] == 1 ){
+                min_idx = Math.min( min_idx, first_idx[i] );
+            }
+        }
+        
+        if( min_idx == Integer.MAX_VALUE )
+            return -1;
+        return min_idx;
+    }
+}
