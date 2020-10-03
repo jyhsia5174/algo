72ae6b1c4f2d067e5c903041ae5136f44534d602
Hsia, Jui-Yang <johncreed@users.noreply.github.com>
2020-10-03 11:24:40 +0800

    Update 203.java

--- java/203.java
+++ java/203.java
@@ -11,6 +11,34 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
+        
+        ListNode prev = sentinel, curr = head;
+        
+        while( curr != null ){
+            if( curr.val == val ) prev.next = curr.next;
+            else prev = curr;
+            curr = curr.next;
+        }
+        
+        return sentinel.next;
+    }
+}
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
         
         ListNode newHead = head;
         while( newHead != null ){
