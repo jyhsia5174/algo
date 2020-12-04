ad6dabc751ac3a7eef10be4695620d092255a8ad
Hsia, Jui-Yang <10295677+jyhsia5174@users.noreply.github.com>
2020-12-04 11:04:27 +0800

    Update 72.cpp

--- cpp/72.cpp
+++ cpp/72.cpp
@@ -36,3 +36,41 @@ public:
 private:
     const int INF = 1<<29;
 };
+
+class Solution {
+public:
+    int minDistance(string s1, string s2) {
+        int n1 = s1.size();
+        int n2 = s2.size();
+        vector<vector<int>> dp( n1+1, vector<int>( n2+1, INF ) );
+        
+        dp[n1][n2] = 0;
+        for( int i = 0; i < n1; i++ )
+            dp[i][n2] = n1 - i;
+        for( int j = 0; j < n2; j++)
+            dp[n1][j] = n2 - j;
+        
+        for( int j = n2-1; j >= 0; j-- ){
+            for( int i = n1-1; i>=0; i-- ){
+                if( s1[i] == s2[j] ){
+                    dp[i][j] = dp[i+1][j+1];
+                }
+                else{
+                    int cnt = INF;
+                    // insert
+                    cnt = min( cnt, 1 + dp[i][j+1] );
+                    // delete
+                    cnt = min( cnt, 1 + dp[i+1][j] );
+                    // replace
+                    cnt = min( cnt, 1 + dp[i+1][j+1] );
+                    dp[i][j] = cnt;
+                }
+            }
+        }
+        
+        return dp[0][0];
+    }
+    
+private:
+    const int INF = 1<<29;
+};
