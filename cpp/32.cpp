79c807400ce127be533f47afe85a369e1313353a
Hsia, Jui-Yang <10295677+jyhsia5174@users.noreply.github.com>
2020-12-24 14:12:50 +0800

    Update 32.cpp

--- cpp/32.cpp
+++ cpp/32.cpp
@@ -40,3 +40,31 @@ public:
     
     unordered_map<int, int> M;
 }; 
+
+
+// Better version
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int res = 0;
+        stack<int> stk;
+        stk.push(-1);
+        
+        for( int i = 0; i < s.size(); i++ ){
+            if( s[i] == '(' ){
+                stk.push( i );
+            }
+            else{
+                stk.pop();
+                if( stk.empty() ){
+                    stk.push(i);
+                }
+                else{
+                    res = max( res, i - stk.top() );
+                }
+            }
+        }
+        
+        return res;
+    }
+}; 
