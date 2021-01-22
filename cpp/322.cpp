a8530f276272f6b02a4aac22bf6d033e66184871
Hsia, Jui-Yang <10295677+jyhsia5174@users.noreply.github.com>
2021-01-22 18:31:43 +0800

    Update 322.cpp

--- cpp/322.cpp
+++ cpp/322.cpp
@@ -1,3 +1,29 @@
+/*
+322. Coin Change
+Runtime: 68 ms, faster than 87.61% of C++ online submissions for Coin Change.
+Memory Usage: 14 MB, less than 70.25% of C++ online submissions for Coin Change.
+*/
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> res(amount+1, INF);
+        res[0] = 0;
+        
+        for(int i = 1; i <= amount; i++){
+            for(auto &val: coins){
+                int pre = i - val;
+                if( pre >= 0 && res[pre] != INF )
+                    res[i] = min( res[i], res[pre] + 1 );
+            }
+        }
+        
+        return (res[amount] == INF)? -1 : res[amount];
+    }
+    
+private:
+    const int INF = numeric_limits<int>::max();
+};
+
 #include <vector>
 #include <limits>
 using namespace std;
