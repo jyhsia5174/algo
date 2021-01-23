0cc884600aef5da7341b707f9e7f45b4dcaacdee
Hsia, Jui-Yang <10295677+jyhsia5174@users.noreply.github.com>
2021-01-23 13:38:06 +0800

    Update 297.cpp

--- cpp/297.cpp
+++ cpp/297.cpp
@@ -1,3 +1,82 @@
+/*
+297. Serialize and Deserialize Binary Tree
+Runtime: 48 ms, faster than 86.35% of C++ online submissions for Serialize and Deserialize Binary Tree.
+Memory Usage: 30.8 MB, less than 94.84% of C++ online submissions for Serialize and Deserialize Binary Tree.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string res = "";
+        dfs( root, res );
+        return res;
+    }
+    
+    void dfs( TreeNode* node, string &res ){
+        if( node == nullptr ){
+            res +="n";
+            return;
+        }
+        
+        res += encodeInt( node->val );
+        dfs( node->left, res);
+        dfs( node->right, res);
+        
+        return;
+    }
+    
+    string encodeInt( int val ){
+        string res = "     " + to_string(val);
+        res = res.substr(res.size() - 5, 5);
+        return res;
+    }
+    
+    int decodeInt( const string &val ){
+        int i = val.find_first_not_of( "0" );
+        return stoi( val.substr( i, val.size() - i ) );
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        int idx = 0;
+        return helper(data, idx);
+    }
+    
+    TreeNode* helper(const string &data, int &idx){
+        assert( idx < data.size() );
+        if( data[idx] == 'n' ){
+            idx++;
+            return nullptr;
+        }
+        
+        int val = decodeInt(data.substr(idx, 5));
+        idx+=5;
+        TreeNode* node = new TreeNode(val);
+        
+        TreeNode* left = helper(data, idx);
+        TreeNode* right = helper(data, idx);
+        
+        node->left = left;
+        node->right = right;
+        
+        return node;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
