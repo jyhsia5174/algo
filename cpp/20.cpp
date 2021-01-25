38ef5b6bc3352e382c7e39efa1cef81537fb74a0
Hsia, Jui-Yang <10295677+jyhsia5174@users.noreply.github.com>
2021-01-25 21:10:09 +0800

    Update 20.cpp

--- cpp/20.cpp
+++ cpp/20.cpp
@@ -1,3 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> stk;
+        unordered_map<char, char> m1;
+        m1['('] = ')';
+        m1['['] = ']';
+        m1['{'] = '}';
+        
+        for(int i = 0; i < s.size(); i++){
+            if( m1.find(s[i]) != m1.end() ){
+                stk.push(s[i]);
+            }
+            else{
+                if( !stk.empty() && m1[stk.top()] == s[i] )
+                    stk.pop();
+                else
+                    return false;
+            }
+        }
+        
+        if( !stk.empty() )
+            return false;
+        return true;
+    }
+};
+
 // 20. Valid Parentheses
 class Solution {
 public:
