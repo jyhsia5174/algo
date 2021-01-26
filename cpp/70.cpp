8a59ed258f5ab063e6d69b88258b9992bbbbc9df
Hsia, Jui-Yang <10295677+jyhsia5174@users.noreply.github.com>
2021-01-26 17:19:24 +0800

    Update 70.cpp

--- cpp/70.cpp
+++ cpp/70.cpp
@@ -1,3 +1,26 @@
+/*
+70. Climbing Stairs
+*/
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> m(n+1, -1);
+        m[0] = 1;
+        m[1] = 1;
+        return helper(n, m);
+    }
+    
+    int helper(int n, vector<int> &m){
+        if( m[n] != -1 )
+            return m[n];
+        
+        m[n] = helper(n-1, m) + helper(n-2, m);
+        
+        return m[n];
+    }
+    
+};
+
 // 70. Climbing Stairs
 class Solution {
 public:
