cbf600314b83ed1da84f0501f0c182e9c03165c5
Hsia, Jui-Yang <10295677+jyhsia5174@users.noreply.github.com>
2021-01-27 20:22:12 +0800

    Create 98.cpp

---
+++ cpp/98.cpp
@@ -0,0 +1,38 @@
+/*
+98. Validate Binary Search Tree
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if( root == nullptr )
+            return true;
+        
+        TreeNode *left = root->left;
+        while( left != nullptr ){
+            if( left->val >= root->val )
+                return false;
+            left = left->right;
+        }
+        
+        TreeNode *right = root->right;
+        while( right != nullptr ){
+            if( right->val <= root->val )
+                return false;
+            right = right->left;
+        }
+        
+        return isValidBST( root->left ) && isValidBST(root->right);
+    }
+};
