cbca056ce4d968d7498f626f918a00417cb0b4c8
Hsia, Jui-Yang <10295677+jyhsia5174@users.noreply.github.com>
2021-02-03 20:52:49 +0800

    Update 307.cpp

--- cpp/307.cpp
+++ cpp/307.cpp
@@ -1,6 +1,61 @@
 /*
 307. Range Sum Query - Mutable
 */
+
+
+class NumArray {
+public:
+    NumArray(vector<int>& nums) {
+        n = nums.size();
+        tree.resize(n+2, 0);
+        
+        for(int i = 0; i < nums.size(); i++)
+            inc(i, 0, nums[i]);
+    }
+    
+    void update(int index, int val) {
+        int preV = sumRange(index, index);
+        inc( index, preV, val);
+    }
+    
+    int sumRange(int left, int right) {
+        return get(right) - get(left-1);
+    }
+    
+    int get(int x){
+        int res = 0;
+        while( x >= 0 ){
+            res += tree[x];
+            x = ( x & (x+1) ) - 1;
+        }
+        return res;
+    }
+    
+    
+    void inc(int x, int preV, int V){
+        int diff = V - preV;
+        
+        while( x < tree.size() ){
+            tree[x] += diff;
+            x = x | (x+1);
+        }
+    }
+    
+    int n;
+    vector<int> tree;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
+
+
+
+
+// old version 
 class NumArray {
 public:
     NumArray(vector<int>& nums) {
