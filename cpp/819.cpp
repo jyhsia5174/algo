39c1bbc6bb2d32d1b8ff3d3072b2f21430a9cd0d
Hsia, Jui-Yang <10295677+jyhsia5174@users.noreply.github.com>
2021-02-05 16:45:21 +0800

    Update 819.cpp

--- cpp/819.cpp
+++ cpp/819.cpp
@@ -35,3 +35,101 @@ public:
         return res;
     }
 };
+
+
+// trie version
+
+/*
+819. Most Common Word
+*/
+
+class node{
+public:
+    node* ch[26];
+    bool matched;
+    node(){
+        for(int i = 0; i < 26; i++)
+            ch[i] = nullptr;
+        matched = false;
+    }
+};
+
+class Solution {
+public:
+    string mostCommonWord(string P, vector<string>& banned) {
+        // build char tree
+        root = new node();
+        init_trie(banned);
+        
+        int i = 0;
+        while( i < P.size() ){
+            string s = get_word(P, i);
+            if( s.size() == 0 )
+                continue;
+            update_freq( s );
+        }
+        
+        string res;
+        int cnt = 0;
+        for(auto &key_val: freq){
+            if( key_val.second > cnt ){
+                res = key_val.first;
+                cnt = key_val.second;
+            }
+        }
+        
+        return res;
+    }
+    
+    bool isBanned(const string &s){
+        node *head = root;
+        bool isBanned = false;
+        for(auto &c: s){
+            int val = c - 'a';
+            if( head->ch[val] == nullptr )
+                return false;
+            else
+                head = head->ch[val];
+        }
+        
+        return head->matched;
+    } 
+    
+    void update_freq(const string &s){
+        if( !isBanned(s) ){
+            freq[s]++;
+        }
+    }
+    
+    string get_word(const string &P, int &i){
+        // skip pucn and white space
+        while(i < P.size() &&  punc.find(P[i]) != punc.end())
+            i++;
+        
+        string res = "";
+        while(i < P.size() && punc.find(P[i]) == punc.end()){
+            res.push_back(tolower(P[i]));
+            i++;
+        }
+        
+        return res;
+    }
+    
+    void init_trie(const vector<string> &banned){
+        for(auto &s: banned){
+            node *head = root;
+            for(auto &c: s){
+                int val = c - 'a';
+                if( head->ch[val] == nullptr )
+                    head->ch[val] = new node();
+                head = head->ch[val];
+            }
+            head->matched = true;
+        }
+    }
+    
+private:
+    node *root;
+    unordered_map<string, int> freq;
+    unordered_set<char> punc{'!', '?', '\'', ',', ';', '.', ' '};
+};
