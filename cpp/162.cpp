0e37bb5783afc618d30ee40e6516f73d247087e3
Hsia, Jui-Yang <10295677+jyhsia5174@users.noreply.github.com>
2021-02-09 16:59:11 +0800

    Update 162.cpp

--- cpp/162.cpp
+++ cpp/162.cpp
@@ -1,3 +1,39 @@
+/*
+162. Find Peak Element
+*/
+/*
+len break   mid lsub    lLen    rsub    rLen
+0   y
+1   y
+2   y
+3   n       1   [0,1]   2       [1,2]   2
+4   n       2   [0,2]   3       [2,3]   2
+5   n       2   
+*/
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if( nums.size() == 1 )
+            return 0;
+        int lo = 0, hi = nums.size() - 1;
+        while( lo + 1 < hi ){
+            int mid = lo + (hi - lo) / 2;
+            if( nums[mid] > nums[mid + 1] )
+                hi = mid;
+            else if( nums[mid] < nums[mid + 1] )
+                lo = mid;
+             else
+                 return -1;
+        }
+        
+        if( nums[hi] > nums[lo] )
+            return hi;
+        else 
+            return lo;
+    }
+};
+
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
