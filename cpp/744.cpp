d384e2c8ace86cdea41cb0843aa7a7b72a497491
Hsia, Jui-Yang <10295677+jyhsia5174@users.noreply.github.com>
2021-02-09 21:01:51 +0800

    Update 744.cpp

--- cpp/744.cpp
+++ cpp/744.cpp
@@ -1,6 +1,59 @@
 /*
 744. Find Smallest Letter Greater Than Target
 */
+
+// template 1
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int lo = 0, hi = letters.size() - 1;
+        int loc = -1;
+        while( lo <= hi ){
+            int mid = lo + (hi - lo) / 2;
+            if( letters[mid] == target )
+                lo = mid + 1;
+            else if( letters[mid] < target )
+                lo = mid + 1;
+            else{
+                loc = mid;
+                hi = mid - 1;
+            }
+        }
+        
+        if( loc != -1 )
+            return letters[loc];
+        // lo - 1 == hi
+        return letters[0];
+    }
+};
+
+// template 2
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int lo = 0, hi = letters.size();
+        int loc = -1;
+        while( lo < hi ){
+            int mid = lo + (hi - lo) / 2;
+            if( letters[mid] == target )
+                lo = mid + 1;
+            else if( letters[mid] < target )
+                lo = mid + 1;
+            else{
+                loc = mid;
+                hi = mid;
+            }
+        }
+        
+        // lo == hi
+        if( loc != -1 )
+            return letters[loc];
+        return letters[0];
+    }
+};
+
+// template 3
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
