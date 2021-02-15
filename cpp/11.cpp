0eb1229de96e7483f60b163d05ec5d151b8be231
Hsia, Jui-Yang <10295677+jyhsia5174@users.noreply.github.com>
2021-02-15 16:54:25 +0800

    Update 11.cpp

--- cpp/11.cpp
+++ cpp/11.cpp
@@ -1,6 +1,32 @@
 /*
 11. Container With Most Water
 */
+
+// n
+class Solution {
+public:
+    int maxArea(vector<int>& H) {
+        const int n = H.size();
+        if( n == 1 || n == 0 )
+            return 0;
+        
+        int lo = 0, hi = n-1;
+        int res = 0;
+        while( lo < hi ){
+            res = max( res, (hi - lo)*min(H[lo], H[hi]) );
+            if( H[lo] <= H[hi] ){
+                lo++;
+            }
+            else{
+                hi--;
+            }
+        }
+        
+        return res;
+    }
+};
+
+// n log n
 class Solution {
 public:
     int maxArea(vector<int>& H) {
