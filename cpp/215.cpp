66cf729ce7062c29d403e8eacc46640d7229360d
Hsia, Jui-Yang <10295677+jyhsia5174@users.noreply.github.com>
2021-02-20 16:30:49 +0800

    Update 215.cpp

--- cpp/215.cpp
+++ cpp/215.cpp
@@ -1,4 +1,74 @@
 // 215. Kth Largest Element in an Array
+class myQ{
+public:
+    myQ(){
+        n = 0;
+        A.resize(1, INT_MIN);
+    }
+    
+    int size(){return n;}
+    
+    void push(int v){
+        while( A.size() <= 2*(n+1) ){
+            A.resize(A.size()*2, INT_MIN);
+        }
+        
+        n++;
+        A[n] = v;
+        int idx = n;
+        while( idx/2 > 0 ){
+            if( A[idx] > A[idx/2] )
+                swap( A[idx], A[idx/2] );
+            idx /= 2;
+        }
+    }
+    
+    void pop(){
+        A[1] = A[n];
+        A[n] = INT_MIN;
+        n--;
+        int i = 1;
+        while( i < (n+1) ){
+            int l = i*2, r = i*2+1;
+            if( A[i] >= max( A[l], A[r] ) )
+                break;
+            
+            if(A[l] >= A[r]){
+                swap(A[i], A[l]);
+                i = l;
+            }
+            else{
+                swap(A[i], A[r]);
+                i = r;
+            }
+        }
+    }
+    
+    int top(){
+        return A[1];
+    }
+    
+private:
+    int n;
+    vector<int> A;
+};
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        myQ Q;
+        
+        for(int i = 0; i < nums.size(); i++){
+            Q.push( -nums[i] );
+            if( Q.size() > k )
+                Q.pop();
+        }
+        
+        return -Q.top();
+    }
+};
+
+
 class Solution {
 public:
     int findKthLargest(vector<int>& A, int k) {
