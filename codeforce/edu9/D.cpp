78f35ee5a8ebaca986c3f499d3a5407c3905bdd1
jyhsia <hsiajuiyang5174@gmail.com>
2021-03-03 14:03:51 +0800

    -

--- codeforce/edu9/D.cpp
+++ codeforce/edu9/D.cpp
@@ -3,6 +3,41 @@ using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(false);
+    
+    int n, m;
+    cin >> n >> m;
+    vector<int> A(n);
+    vector<int> dp(m+1, 0);
+    for(auto &a: A){
+        cin >> a;
+        if( a <= m )
+            dp[a]++;
+    }
+
+    vector<int> divCnt(m+1, 0);
+    for(int i = 1; i <= m; i++){
+        for(int j = i; j <= m; j+=i)
+            divCnt[j] += dp[i];
+    }
+
+    int lcm = 1;
+    for(int i = 2; i <= m; i++)
+        if( divCnt[i] > divCnt[lcm] )
+            lcm = i;
+
+    cout << lcm << " " << divCnt[lcm] << endl;
+    for(int i = 0; i < n; i++){
+        if( lcm % A[i] == 0 )
+            cout << i+1 << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
+
+/*
+int main(){
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
@@ -46,3 +81,4 @@ int main(){
 
     return 0;
 }
+*/
