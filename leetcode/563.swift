62c0853b90efa57c69bdc7b7fecbdf18422364fe
Hsia, Jui-Yang (夏睿陽) <10295677+jyhsia5174@users.noreply.github.com>
2021-09-29 22:56:04 +0800

    Update and rename 563.swift to leetcode/563.swift

--- leetcode/563.swift
+++ leetcode/563.swift
@@ -14,6 +14,37 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func findTilt(_ root: TreeNode?) -> Int {
+        var res = 0
+        sum(root, &res)
+        return res
+    }
+    
+    func sum(_ root: TreeNode?, _ res: inout Int) -> Int {
+        if root == nil { return 0 }
+        var leftsum = sum(root!.left, &res)
+        var rightsum = sum(root!.right, &res)
+        res += abs(leftsum - rightsum)
+        return root!.val + leftsum + rightsum
+    }
+}
+
 class Solution {
     var totalTilt = 0
     
