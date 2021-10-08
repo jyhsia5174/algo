b89665fafb8a2864cf2097dc1b4c7d8d13718faf
Hsia, Jui-Yang (夏睿陽) <10295677+jyhsia5174@users.noreply.github.com>
2021-10-08 16:27:23 +0800

    Update 1334.swift

--- leetcode/1334.swift
+++ leetcode/1334.swift
@@ -41,3 +41,42 @@ class Solution {
         return city
     }
 }
+
+
+
+// 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance
+class Solution {
+    func findTheCity(_ n: Int, _ edges: [[Int]], _ distanceThreshold: Int) -> Int {
+        var dp : [[Int]] = Array(repeating: Array(repeating: Int.max/2, count: n), count: n)
+        
+        for edge in edges {
+            dp[edge[0]][edge[1]] = edge[2]
+            dp[edge[1]][edge[0]] = edge[2]
+        }
+        
+        for i in 0..<n {
+            dp[i][i] = 0
+        }
+        
+        for k in 0..<n {
+            for i in 0..<n {
+                for j in 0..<n {
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j])
+                }
+            }
+        }
+        
+        var neighborCount: [Int] = dp.flatMap{ $0.filter{ $0 <= distanceThreshold }.count }
+        
+        var city = 0
+        var min_count = neighborCount[city]
+        for i in 1..<n {
+            if neighborCount[i] <= min_count {
+                city = i
+                min_count = neighborCount[i]
+            }
+        }
+        
+        return city
+    }
+}
